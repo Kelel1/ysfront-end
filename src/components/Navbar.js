e00aab8f34bd1d1b6562ffc891d2b1b3fb862833
Kern Elder <kelel1@yahoo.com>
2022-04-29 14:56:52 -0400

    uninstall bootstap/react-bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,9 @@
 import '../App.css'
-import { Navbar, Nav, Container } from 'react-bootstrap';
 const Navbars = () => {
 
     return (
        
-        <Navbar bg="yardGreen" variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">YardSold </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    <Nav.Link href="#home">Sign Up</Nav.Link>
-                    <Nav.Link href="#link">Sign In</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <h1>Hello World</h1>
     )
 
 }
