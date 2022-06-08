75412f62e9ae04a07f959f58ad820ff003c7a7d2
Kern Elder <kelel1@yahoo.com>
2022-06-07 20:41:42 -0400

    update/improve log-in form

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,13 +1,42 @@
-import { Input, Stack } from '@chakra-ui/react';
+import { Input, Stack, Button, Box } from '@chakra-ui/react';
+import { useState }     from 'react';
 
 const Login = () => {
+
+    const[token, setToken] = useState(null);
+
+    if (!token) {
+        return (
+           <Box border='1px' borderColor='#51bc31' mt={40} mr={40} ml={40} width={410}>
+                <div> 
+                    <Stack spacing={20} py={5} px={20} >
+                        <p>
+                        Sorry, user login necessary.
+                        </p>
+                    </Stack>
+
+                    <Stack spacing={1} width={80} ml={10} mb={10}>
+                        <p>Username</p>
+                            <Input variant='filled'/>
+                        <p>Password</p>
+                            <Input variant='filled'/>
+                    </Stack>
+                    <Button borderRightRadius="0" ml={40} mb={5}>Sign In</Button>
+                </div>
+             </Box>
+        )
+    }
     return (
-        <Stack spacing={5} htmlSize={5} width={5}>
-            <h4>Username</h4>
-                <Input variant='filled'/>
-            <h4>Password</h4>
-                <Input variant='filled'/>
-        </Stack>
+        <Box border='1px' borderColor='#51bc31'>
+        <div> 
+            <Stack spacing={1} width={60} px={20}>
+                <h4>Username</h4>
+                    <Input variant='filled'/>
+                <h4>Password</h4>
+                    <Input variant='filled'/>
+            </Stack>
+        </div>
+     </Box>
     )
 }
 
