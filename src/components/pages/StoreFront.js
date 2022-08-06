24cf2f12490eab095c02994b8f6b31185641bc4e
Kern Elder <kelel1@yahoo.com>
2022-08-06 13:16:31 -0400

    add fetchItems query to StoreFront component

--- src/components/pages/StoreFront.js
+++ src/components/pages/StoreFront.js
@@ -1,4 +1,20 @@
+import   Card            from '../Card';
+import { gql, useQuery } from '@apollo/client';
 
+const FETCH_ITEMS = gql`
+query {
+    fetchItems {
+        name,
+        price,
+        inventoryCount,
+        images,
+        description,
+        onHold,
+        totalOnHold,
+        id
+    }
+}
+`
 
 const StoreFront = () => {
 
@@ -7,6 +23,7 @@ const StoreFront = () => {
         <h2>
             Welcome to your Store Front [User]
         </h2>
+        <Card></Card>
     </div>
    )
 
